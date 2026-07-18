f079afdc4f1e6771b696376ab6227aedbf00f460
csrlop26 <csrlop26@gmail.com>
2026-07-18 22:30:51 +0200

    Deploy: disable ChromaticWaves overlay on mobile

--- assets/ProjectModal-C6caAQ99.js
+++ assets/ProjectModal-C6caAQ99.js
@@ -1,4 +1,4 @@
-import{f as c,r as l,j as e,a as f,m as i,b as o,X as x,d}from"./index-DOpnkQWq.js";import{C as h}from"./calendar-DMUCDHi6.js";/**
+import{f as c,r as l,j as e,a as f,m as i,b as o,X as x,d}from"./index-co3VR6Fb.js";import{C as h}from"./calendar-HX14i5CT.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/calendar-HX14i5CT.js
+++ assets/calendar-HX14i5CT.js
@@ -1,4 +1,4 @@
-import{f as e}from"./index-DOpnkQWq.js";/**
+import{f as e}from"./index-co3VR6Fb.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
